f234eb8732b4ca37bb969a11b17ba69124d0c326
cadenkoehl <caden@etherscapes.com>
2021-03-15 14:53:54 -1000

    Added Exception handling to CommandHandler

--- src/main/java/net/thetowncraft/townbot/api/command_handler/CommandHandler.java
+++ src/main/java/net/thetowncraft/townbot/api/command_handler/CommandHandler.java
@@ -6,6 +6,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.thetowncraft.townbot.Bot;
 import net.thetowncraft.townbot.api.command_handler.discord.DiscordCommand;
 import net.thetowncraft.townbot.api.command_handler.minecraft.MinecraftCommand;
+import net.thetowncraft.townbot.util.Constants;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,14 +19,19 @@ public class CommandHandler {
     public static class Minecraft implements Listener {
         @EventHandler
         public void onCommand(PlayerCommandPreprocessEvent event) {
-            for(MinecraftCommand cmd : MinecraftCommand.COMMANDS) {
-                if(event.getMessage().equals(cmd.getName())) {
-                    if(!event.getPlayer().isOp() && cmd.isAdminCommand()) {
-                        event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to use this command!");
-                        return;
+            try {
+                for(MinecraftCommand cmd : MinecraftCommand.COMMANDS) {
+                    if(event.getMessage().equals(cmd.getName())) {
+                        if(!event.getPlayer().isOp() && cmd.isAdminCommand()) {
+                            event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                            return;
+                        }
                     }
-                    cmd.execute(new CommandEvent.Minecraft(event, cmd));
                 }
+            }
+            catch (Exception ex) {
+                Bukkit.getServer().broadcastMessage("[ERROR]: " + ChatColor.RED + ex);
+                Constants.DEV_CHAT.sendMessage(":x: [**" + Constants.DEV_ROLE.getAsMention() + "**] **An exception has occurred with a Minecraft command!** `" + ex + "`").queue();
             }
         }
     }
@@ -32,24 +39,28 @@ public class CommandHandler {
     public static class Discord extends ListenerAdapter {
         @Override
         public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+            try {
+                if(event.isWebhookMessage()) return;
+                if(event.getAuthor().isBot()) return;
 
-            if(event.isWebhookMessage()) return;
-            if(event.getAuthor().isBot()) return;
+                String[] args = event.getMessage().getContentRaw().split("\\s+");
 
-            String[] args = event.getMessage().getContentRaw().split("\\s+");
+                Member member = event.getMember();
+                if(member == null) return;
 
-            Member member = event.getMember();
-            if(member == null) return;
+                for(DiscordCommand cmd : DiscordCommand.COMMANDS) {
 
-            for(DiscordCommand cmd : DiscordCommand.COMMANDS) {
+                    if(!args[0].equalsIgnoreCase(Bot.prefix + cmd.getName())) continue;
 
-                if(!args[0].equalsIgnoreCase(Bot.prefix + cmd.getName())) continue;
-
-                if(!member.hasPermission(cmd.getRequiredPermission())) {
-                    event.getChannel().sendMessage(":x: You must have the **" + cmd.getRequiredPermission().getName() + "** permission to use this command!").queue();
-                    return;
+                    if(!member.hasPermission(cmd.getRequiredPermission())) {
+                        event.getChannel().sendMessage(":x: You must have the **" + cmd.getRequiredPermission().getName() + "** permission to use this command!").queue();
+                        return;
+                    }
+                    cmd.execute(new CommandEvent.Discord(event, cmd));
                 }
-                cmd.execute(new CommandEvent.Discord(event, cmd));
+            }
+            catch (Exception ex) {
+                Constants.DEV_CHAT.sendMessage(":x: [**" + Constants.DEV_ROLE.getAsMention() + "**] **An exception has occurred with a Minecraft command!** `" + ex + "`").queue();
             }
         }
     }
