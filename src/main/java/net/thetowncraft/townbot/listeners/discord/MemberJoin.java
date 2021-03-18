0edb26ad8300cc5f537ac3fc11ed8ea71905ff4c
Sourcecotton <coalforthemonsters@gmail.com>
2021-03-17 18:57:56 -0600

    Removed "An_Unlinked_Player" nickname given to players upon them joining the server

--- src/main/java/net/thetowncraft/townbot/listeners/discord/MemberJoin.java
+++ src/main/java/net/thetowncraft/townbot/listeners/discord/MemberJoin.java
@@ -12,8 +12,6 @@ public class MemberJoin extends ListenerAdapter {
         event.getGuild().addRoleToMember(event.getMember(), Constants.PINGS_CATEGORY_ROLE).queue();
         event.getGuild().addRoleToMember(event.getMember(), Constants.OTHER_CATEGORY_ROLE).queue();
 
-        event.getMember().modifyNickname("An_Unlinked_Player").queue();
-
         event.getUser().openPrivateChannel().queue(channel -> channel.sendMessage("Heyo! Welcome to **The Town**! If you have any questions or concerns about the server, DM me! Your message is sent straight to the staff! :)").queue());
     }
 }
