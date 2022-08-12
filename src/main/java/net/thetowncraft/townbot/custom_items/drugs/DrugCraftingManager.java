c060d71160468d457d288b94bdf6373fb0301280
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 21:42:38 -0600

    Made the NamespacedKey names lowercase

--- src/main/java/net/thetowncraft/townbot/custom_items/drugs/DrugCraftingManager.java
+++ src/main/java/net/thetowncraft/townbot/custom_items/drugs/DrugCraftingManager.java
@@ -24,23 +24,23 @@ public class DrugCraftingManager {
 
         //Basic Ingredient Recipes
 
-        NETHER_CANDY_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "netherCandyRecipe"), CustomItems.NETHER_CANDY.createItemStack(1));
+        NETHER_CANDY_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "nether_candy_recipe"), CustomItems.NETHER_CANDY.createItemStack(1));
         NETHER_CANDY_RECIPE.shape("SNS","SNS","SNS");
         NETHER_CANDY_RECIPE.setIngredient('S', Material.SUGAR);
         NETHER_CANDY_RECIPE.setIngredient('N', Material.NETHER_WART);
 
-        SLEEPER_RECIPE = new SmithingRecipe(new NamespacedKey(Plugin.get(), "sleeperRecipe"),
+        SLEEPER_RECIPE = new SmithingRecipe(new NamespacedKey(Plugin.get(), "sleeper_recipe"),
                 CustomItems.SLEEPER.createItemStack(1), new RecipeChoice.MaterialChoice(Material.FERMENTED_SPIDER_EYE),
                 new RecipeChoice.ExactChoice(CustomItems.NETHER_CANDY.createItemStack(1)));
 
-        SPICE_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "spiceRecipe"),
+        SPICE_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "spice_recipe"),
                 CustomItems.SPICE.createItemStack(1));
         SPICE_RECIPE.shape("NBN","BNB","NBN");
 
         SPICE_RECIPE.setIngredient('B', Material.BLAZE_POWDER);
         SPICE_RECIPE.setIngredient('N', new RecipeChoice.ExactChoice(CustomItems.NETHER_CANDY.createItemStack(1)));
 
-        SWEET_DUST_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "sweetDustRecipe"),
+        SWEET_DUST_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "sweet_dust_recipe"),
                 CustomItems.SWEET_DUST.createItemStack(1));
         SWEET_DUST_RECIPE.shape("SSS","NNN","SSS");
         SWEET_DUST_RECIPE.setIngredient('S', Material.SUGAR);
@@ -48,18 +48,18 @@ public class DrugCraftingManager {
 
         //Meth Ingredients Recipes
 
-        POWER_MIX_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "powerMixRecipe"),
+        POWER_MIX_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "power_mix_recipe"),
                 CustomItems.POWER_MIX.createItemStack(1));
         POWER_MIX_RECIPE.shape(" S ","BWB"," S ");
         POWER_MIX_RECIPE.setIngredient('S', Material.SUGAR);
         POWER_MIX_RECIPE.setIngredient('B', new RecipeChoice.ExactChoice(CustomItems.SPICE.createItemStack(1)));
         POWER_MIX_RECIPE.setIngredient('W', new RecipeChoice.ExactChoice(CustomItems.SLEEPER.createItemStack(1)));
 
-        COOKED_POWER_MIX_RECIPE = new FurnaceRecipe(new NamespacedKey(Plugin.get(), "cookedPowerMixRecipe"),
+        COOKED_POWER_MIX_RECIPE = new FurnaceRecipe(new NamespacedKey(Plugin.get(), "cooked_power_mix_recipe"),
                 CustomItems.COOKED_POWER_MIX.createItemStack(1),
                 new RecipeChoice.ExactChoice(CustomItems.POWER_MIX.createItemStack(1)), 1f, 400);
 
-        HYPER_SUGAR_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "hypersugarRecipe"),
+        HYPER_SUGAR_RECIPE = new ShapedRecipe(new NamespacedKey(Plugin.get(), "hypersugar_recipe"),
                 CustomItems.HYPERSUGAR.createItemStack(1));
         HYPER_SUGAR_RECIPE.shape(" S ","SBS"," S ");
         HYPER_SUGAR_RECIPE.setIngredient('S', new RecipeChoice.ExactChoice(CustomItems.SWEET_DUST.createItemStack(1)));
@@ -67,14 +67,14 @@ public class DrugCraftingManager {
 
         //Powdered Meth, Crystal Meth and Refined Meth Recipes
 
-        POWDERED_METH_RECIPE = new SmithingRecipe(new NamespacedKey(Plugin.get(), "powderedMethRecipe"),
+        POWDERED_METH_RECIPE = new SmithingRecipe(new NamespacedKey(Plugin.get(), "powdered_meth_recipe"),
                 CustomItems.POWDERED_METH.createItemStack(1), new RecipeChoice.ExactChoice(CustomItems.COOKED_POWER_MIX.createItemStack(1)),
                 new RecipeChoice.ExactChoice(CustomItems.HYPERSUGAR.createItemStack(1)));
 
-        CRYSTAL_METH_RECIPE = new FurnaceRecipe(new NamespacedKey(Plugin.get(), "crystalMethRecipe"),
+        CRYSTAL_METH_RECIPE = new FurnaceRecipe(new NamespacedKey(Plugin.get(), "crystal_meth_recipe"),
                 CustomItems.CRYSTAL_METH.createItemStack(1), new RecipeChoice.ExactChoice(CustomItems.POWDERED_METH.createItemStack(1)), 1.5f, 800);
 
-        REFINED_METH_RECIPE = new BlastingRecipe(new NamespacedKey(Plugin.get(), "refinedMethRecipe"),
+        REFINED_METH_RECIPE = new BlastingRecipe(new NamespacedKey(Plugin.get(), "refined_meth_recipe"),
                 CustomItems.REFINED_METH.createItemStack(1), new RecipeChoice.ExactChoice(CustomItems.CRYSTAL_METH.createItemStack(1)), 1.5f, 1600);
 
 
