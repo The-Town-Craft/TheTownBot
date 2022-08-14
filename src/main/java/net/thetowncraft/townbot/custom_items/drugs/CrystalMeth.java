bfdf2d2eeb28b1407cf7e3b1065e20f1021197ea
BuildTools <unconfigured@null.spigotmc.org>
2022-08-14 00:11:59 -0600

    Fixed up a few things in CrystalMeth class

--- src/main/java/net/thetowncraft/townbot/custom_items/drugs/CrystalMeth.java
+++ src/main/java/net/thetowncraft/townbot/custom_items/drugs/CrystalMeth.java
@@ -7,44 +7,39 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffectType;
+import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.File;
 import java.util.HashMap;
-import java.util.UUID;
-
-import static net.thetowncraft.townbot.util.data.Data.getFile;
 
 public class CrystalMeth extends CustomItem {
-
-    private static final HashMap<UUID, Integer> METH_STAGES = new HashMap<>();
-    private static final HashMap<UUID, Long> WITHDRAWAL_TIMER = new HashMap<>();
-    private static final HashMap<UUID, Boolean> AWAITING_SYMPTOMS = new HashMap<>();
+    private static final HashMap<String, Integer> METH_STAGES = new HashMap<>();
+    private static final HashMap<String, Long> WITHDRAWAL_TIMER = new HashMap<>();
+    private static final HashMap<String, Boolean> AWAITING_SYMPTOMS = new HashMap<>();
 
     @Override
     public void onInteract(PlayerInteractEvent event, int amount){
         Player player = event.getPlayer();
         double randomTime = Math.random() * (180000.0 - 60000) + 60000;
 
-        if(!METH_STAGES.containsKey(player.getUniqueId())){
+        if(!METH_STAGES.containsKey(player.getUniqueId().toString())){
             doFirstTimeEffects(player);
         }
 
-        if(METH_STAGES.containsKey(player.getUniqueId())){
+        if(METH_STAGES.containsKey(player.getUniqueId().toString())){
             doNonMethVirginEffects(player);
         }
 
-        if(METH_STAGES.get(player.getUniqueId()) > 1){
-            if(!WITHDRAWAL_TIMER.containsKey(player.getUniqueId())){
-                WITHDRAWAL_TIMER.put(player.getUniqueId(), System.currentTimeMillis());
-                AWAITING_SYMPTOMS.put(player.getUniqueId(), true);
+        if(METH_STAGES.get(player.getUniqueId().toString()) > 1){
+            if(!WITHDRAWAL_TIMER.containsKey(player.getUniqueId().toString())){
+                WITHDRAWAL_TIMER.put(player.getUniqueId().toString(), System.currentTimeMillis());
+                AWAITING_SYMPTOMS.put(player.getUniqueId().toString(), true);
 
-                while(!AWAITING_SYMPTOMS.get(player.getUniqueId())){
-                    if(System.currentTimeMillis() - WITHDRAWAL_TIMER.get(player.getUniqueId()) > randomTime){
-                        AWAITING_SYMPTOMS.replace(player.getUniqueId(), false);
-
+                while(!AWAITING_SYMPTOMS.get(player.getUniqueId().toString())){
+                    if(System.currentTimeMillis() - WITHDRAWAL_TIMER.get(player.getUniqueId().toString()) > randomTime){
+                        AWAITING_SYMPTOMS.replace(player.getUniqueId().toString(), false);
+                        doWithdrawalSymptoms(player);
                     }
                 }
             }
@@ -56,7 +51,7 @@ public class CrystalMeth extends CustomItem {
     }
 
     public void doFirstTimeEffects(Player player){
-        METH_STAGES.put(player.getUniqueId(), 1);
+        METH_STAGES.put(player.getUniqueId().toString(), 1);
         player.addPotionEffect(PotionEffectType.CONFUSION.createEffect(600, 1));
         player.addPotionEffect(PotionEffectType.SPEED.createEffect(600, 20));
         player.addPotionEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(800, 2));
@@ -64,24 +59,23 @@ public class CrystalMeth extends CustomItem {
     }
 
     public void doNonMethVirginEffects(Player player){
-        int dampener = METH_STAGES.get(player.getUniqueId());
-        METH_STAGES.replace(player.getUniqueId(), METH_STAGES.get(player.getUniqueId()) + 1);
+        METH_STAGES.replace(player.getUniqueId().toString(), METH_STAGES.get(player.getUniqueId().toString()) + 1);
         player.addPotionEffect(PotionEffectType.CONFUSION.createEffect(600, 1));
-        player.addPotionEffect(PotionEffectType.SPEED.createEffect(600, 20 - METH_STAGES.get(player.getUniqueId()) * 2));
+        player.addPotionEffect(PotionEffectType.SPEED.createEffect(600, 20 - METH_STAGES.get(player.getUniqueId().toString()) * 2));
         player.addPotionEffect(PotionEffectType.INCREASE_DAMAGE.createEffect(800, 2));
-        player.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(1200, 5 - METH_STAGES.get(player.getUniqueId()) / 2));
+        player.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(1200, 5 - METH_STAGES.get(player.getUniqueId().toString()) / 2));
     }
 
-    public void doWithdrawlSymptoms(Player player){
+    public void doWithdrawalSymptoms(Player player){
         player.addPotionEffect(PotionEffectType.CONFUSION.createEffect(1200, 2));
-        if(METH_STAGES.get(player.getUniqueId()) > 2){
+        if(METH_STAGES.get(player.getUniqueId().toString()) > 2){
             player.addPotionEffect(PotionEffectType.BLINDNESS.createEffect(750, 1));
             player.addPotionEffect(PotionEffectType.WEAKNESS.createEffect(3600, 1));
         }
-        if(METH_STAGES.get(player.getUniqueId()) > 3){
-            player.addPotionEffect(PotionEffectType.SLOW.createEffect(200 * METH_STAGES.get(player.getUniqueId()), METH_STAGES.get(player.getUniqueId())));
+        if(METH_STAGES.get(player.getUniqueId().toString()) > 3){
+            player.addPotionEffect(PotionEffectType.SLOW.createEffect(200 * METH_STAGES.get(player.getUniqueId().toString()), METH_STAGES.get(player.getUniqueId().toString())));
         }
-        if(METH_STAGES.get(player.getUniqueId()) > 5){
+        if(METH_STAGES.get(player.getUniqueId().toString()) > 5){
             player.addPotionEffect(PotionEffectType.WITHER.createEffect(200, 1));
         }
         player.sendMessage(ChatColor.RED + "You are suffering withdrawal symptoms...");
@@ -93,10 +87,20 @@ public class CrystalMeth extends CustomItem {
         Data.writeToFile(getFile(), methStages.toString());
     }
 
+    public static void load() {
+        JSONObject methStages = Data.getJSONObjectFromFile(getFile());
+        for(String key : methStages.keySet()) {
+            try {
+                METH_STAGES.put(key, (Integer) methStages.get(key));
+            }
+            catch (JSONException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
     private static File getFile() {
         return Data.getFile("drugs/crystalmeth/methstages.json");
-        return Data.getFile("drugs/crystalmeth/withdrawaltimer.json");
-        return Data.getFile("drugs/crystalmeth/withdrawal.json");
     }
 
     @Override
