cf6bdbc6f7dd39955b8ac5457afdc9b0983aa2cc
Caden Koehl <caden@etherscapes.com>
2022-08-13 16:06:33 -1000

    Finished BountyManager

--- src/main/java/net/thetowncraft/townbot/factions/teams/bounty/BountyManager.java
+++ src/main/java/net/thetowncraft/townbot/factions/teams/bounty/BountyManager.java
@@ -1,6 +1,9 @@
 package net.thetowncraft.townbot.factions.teams.bounty;
 
+import net.dv8tion.jda.api.entities.Member;
+import net.thetowncraft.townbot.listeners.accountlink.AccountManager;
 import net.thetowncraft.townbot.util.data.Data;
+import org.bukkit.OfflinePlayer;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,7 +12,29 @@ import java.util.HashMap;
 
 public class BountyManager {
 
-    public static final HashMap<String, Integer> BOUNTIES = new HashMap<>();
+    private static final HashMap<String, Integer> BOUNTIES = new HashMap<>();
+
+    public void setBounty(OfflinePlayer player, int amount) {
+        BOUNTIES.put(player.getUniqueId().toString(), amount);
+    }
+
+    public void setBounty(Member member, int amount) {
+        OfflinePlayer player = AccountManager.getInstance().getMinecraftPlayer(member);
+        if(player == null) return;
+        setBounty(player, amount);
+    }
+
+    public int getBounty(OfflinePlayer player) {
+        Integer bounty = BOUNTIES.get(player.getUniqueId().toString());
+        if(bounty == null) return 0;
+        return bounty;
+    }
+
+    public int getBounty(Member member) {
+        OfflinePlayer player = AccountManager.getInstance().getMinecraftPlayer(member);
+        if(player == null) return 0;
+        return getBounty(player);
+    }
 
     public static void save() {
         JSONObject bounties = new JSONObject();
